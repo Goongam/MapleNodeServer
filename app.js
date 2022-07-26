98c6b286566dc2d60ba04daeb875c3f56c3700b0
Goongam <dlwjddn341@naver.com>
2022-07-27 01:01:20 +0900

    db연동

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 const cors = require('cors');
+const oracledb = require('oracledb');
 
 app.use(cors({ //모든 요청에 대해 특정 미들웨어를 적용하고 싶다면 use를 이용합니다.
     origin: '*', //모든 출처허용 true도 가능
@@ -46,25 +47,52 @@ const getData = async (callback, id)=>{
         
         return 1;
     } catch (error) {
-        console.log("ErrorID:"+id);
+        console.log("ErrorID1:"+id);
        return 0;
     }
 }
 
 
+let connection;
+async function connectDB(){
+    oracledb.outFormat = oracledb.OUT_FORMAT_OBJECT;
+    connection = await oracledb.getConnection({
+    user          : "ljw",
+    password      : "123123",
+    connectString : "xe"
+    });
+}
+async function getIdbyNick(nick){
+    try {
+        const result = await connection.execute("select id from MapleIdList where nickname = :nick",[nick]);
+        return result.rows[0].ID;
+    } catch (error) {
+        return error;
+    }
+}
 
 
 
 app.post('/data', async function(req, res){
     //res.header("Access-Control-Allow-Origin", "*");
 
-    const reqId = req.body.id;
+    const reqNick = req.body.Nick;
+    let reqId = await getIdbyNick(reqNick);
+
+    if(isNaN(reqId)) {
+        console.log("에러:",reqId);
+        res.send({"error":"없는 닉네임 or DB연결 오류"});
+        return;
+    }
+
+    console.log(reqId);
 
     let msg = await getData( (XML) => {
         const body = JSON.parse(XML);
         const data = body['soap:Envelope']['soap:Body']['GetCharacterInfoByAccountIDResponse']['GetCharacterInfoByAccountIDResult']['diffgr:diffgram']['NewDataSet']['UserInfo'];
         CharData = data['CharacterName']['_text'];
-        res.send(`{ "data" : "${data['CharacterName']['_text']}" }`);
+        console.log(data);
+        res.send(data);
     }, reqId);
 
     if(!msg) res.send(`{ "data" : "실패" }`);
@@ -75,4 +103,5 @@ app.post('/data', async function(req, res){
 
 app.listen(3001, function(){
     console.log("start!!");
+    connectDB();
 });
